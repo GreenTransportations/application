b296cd167287625cba5139a4d8e92db40392f149
Alex Leye <alex.leye@monash.edu>
2021-07-17 12:28:49 +1000

    User model

---
+++ server/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+    lastname:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+    DOB:{
+        type:Date,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+});
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
