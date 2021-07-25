174d86898938f3b85c18f4c482024a03d528674b
Alexander Leye <anley1@student.monash.edu>
2021-07-25 17:02:52 +1000

    Add login route to mainpage

--- webapp/src/pages/main/main.page.jsx
+++ webapp/src/pages/main/main.page.jsx
@@ -14,6 +14,7 @@ import Header from '../../components/header/header.component';
 import DashboardPage from '../dashboard/dashboard.page';
 import MapPage from '../navigation/map.page';
 import TestPage from '../test/test.page';
+import LoginPage from '../authentication/login.page';
 
 
 // Style
@@ -41,6 +42,9 @@ const MainPage = ({id}) => {
                     </Route>
                     <Route path="/tests">
                         <TestPage />         
+                    </Route>
+                    <Route path="/login">
+                        <LoginPage />         
                     </Route>
                     <Route path="/">
                         <DashboardPage />                    
