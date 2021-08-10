7fe09b3e294049b56a179d2a7cad129345ba4922
Alexander Leye <anley1@student.monash.edu>
2021-08-10 12:42:16 +1000

    Add GVM/GCM to model

--- server/models/vehicle.model.js
+++ server/models/vehicle.model.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-// Declare the Schema of the Mongo model
+/*  Declare the Schema of the Mongo model for Vehicle
+    This model will be for all the trucks found in data research
+    that have a fuel effiicency data and statistics.
+*/
 var vehicleSchema = new mongoose.Schema({
     make:{
         type:String,
@@ -22,6 +25,14 @@ var vehicleSchema = new mongoose.Schema({
     fuel_eff:{
         // fuel efficiency in L/km
         type:String,
+        required:true,
+    },
+    gvm:{
+        type:Number,
+        required:true,
+    },
+    gcm:{
+        type:Number,
         required:true,
     },
 });
