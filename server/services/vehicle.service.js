d9c7a1b95f8481b7a9dc9f9aa5708d5fed98ba16
Alexander Leye <anley1@student.monash.edu>
2021-08-12 16:04:01 +1000

    Vehicle service updated

--- server/services/vehicle.service.js
+++ server/services/vehicle.service.js
@@ -1,7 +1,7 @@
 // Services for the Vehicle Model
 const express = require("express");
 const mongoose = require("mongoose");
-const vehicleService = express.Router();
+const vehicleServices = express.Router();
 
 // Configs, Utilities and Enums
 const config = require("../configs/server.config");
@@ -16,18 +16,18 @@ connection.once('open', () => {
 
 
 // Mongoose Model [Vehicle]
-let User = require("../models/vehicle.model");
+let Vehicle = require("../models/vehicle.model");
 
 
 /* All Vehicles 
     Display the information of all vehicles in the Db.
 */
 // ../vehicle/all
-vehicleService.route("/all").get((_, res) => {
+vehicleServices.route("/all").get((_, res) => {
     console.log("/vehicle/all", "GET");
     
     // Find all vehicles
-    Test.find({}, (err, vehicles) => {
+    Vehicle.find({}, (err, vehicles) => {
         if (err) {
             console.log("/vehicle/all", err);
             res.status(500).json({
@@ -40,3 +40,79 @@ vehicleService.route("/all").get((_, res) => {
     });
 })
 
+/* Specific Vehicle GET
+    Get a specific vehicle by its ID.
+*/
+vehicleServices.route("/:id").get((req, res) => {
+    console.log("/vehicle/:id", "GET");
+
+    // Find specific vehicle
+    Vehicle.findById(req.params.id, (err, vehicle) => {
+        if (err) {
+            console.log("/vehicle/:id", err);
+            res.status(500).json({
+                err: "Error in finding this vehicle"
+            });
+        } else {
+            res.status(200).json(vehicle);
+        }
+    });
+})
+
+/* Create a new Vehicle
+    A user is able to create a new Vehicle in this POST request.
+*/
+vehicleServices.route("/create").post((req, res) => {
+    console.log("/vehicle/create", "POST");
+
+    // Create new Vehicle following the model
+    try{
+        const newVehicle = new Vehicle({
+            make: req.body.make.trim(),
+            model: req.body.model.trim(),
+            date: new Date(req.body.date.trim()),
+            reg_no: req.body.reg_no.trim(),
+            fuel_eff: req.body.fuel_eff.trim(),
+            gvm: req.body.gvm.trim(),
+            gcm: req.body.gcm.trim()
+        });
+    
+        newVehicle.save();
+    }
+    catch(e){
+        console.log("/vehicle/create/", e);
+        res.status(500).json({
+            err: "Cannot create new Vehicle"
+        });
+    }
+
+    res.status(200).json(newUser);
+})
+
+// Update Vehicle of :id
+vehicleServices.route("/:id").post((req, res) => {
+    console.log("/vehicle/:id", "POST");
+
+    // Update any Vehicle
+    Vehicle.findByIdAndUpdate(req.params.id, { 
+        make: req.body.make.trim(),
+        model: req.body.model.trim(),
+        date: new Date(req.body.date.trim()),
+        reg_no: req.body.reg_no.trim(),
+        fuel_eff: req.body.fuel_eff.trim(),
+        gvm: req.body.gvm.trim(),
+        gcm: req.body.gcm.trim()
+    }, (err, vehicle) => {
+        if (err) {
+            console.log("/vehicle/:id", err);
+            res.status(500).json({
+                err: "Error in finding Vehicle with this ID"
+            });
+            
+        } else {
+            res.status(200).json(vehicle);
+        }
+    });
+})
+
+module.exports = vehicleServices;
