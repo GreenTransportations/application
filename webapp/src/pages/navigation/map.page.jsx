30a3cbdac2aceaf1e64821734e6987c23055c3b0
Alexander Leye <anley1@student.monash.edu>
2021-08-23 21:06:19 +1000

    Initial Trip Select View

--- webapp/src/pages/navigation/map.page.jsx
+++ webapp/src/pages/navigation/map.page.jsx
@@ -3,7 +3,15 @@ import React, { useState } from 'react';
 
 // Material UI Core Components
 import { makeStyles } from '@material-ui/core/styles';
+import { Button, Grid } from '@material-ui/core';
+
+// API Key for Google Maps
 import { API_KEY } from '../../data/api.key';
+
+// Material UI Icons
+import AddIcon from '@material-ui/icons/Add';
+import { TextField } from '@material-ui/core';
+
 
 
 // Material UI Icons
@@ -17,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     iframe: {
         border:0,
         width:"100%",
-        height:"100%",
+        height:"90vh",
     }
 }));
 
@@ -25,13 +33,65 @@ const useStyles = makeStyles((theme) => ({
 const MapPage = () => {
     const classes = useStyles();
     return (
-        <iframe
-            className={classes.iframe}
-            loading="lazy"
-            allowFullScreen
-            src={`https://www.google.com/maps/embed/v1/view?key=${API_KEY.GOOGLE}
-            &center=-33.8569,151.2152`}>
-        </iframe>
+        <div
+        style={{padding: "30px"}}
+        >
+        <Grid
+            container
+            direction="row"
+            justify="space-around"
+            alignItems="center"
+            spacing={1}
+        >
+
+            <Grid 
+                item
+                xs={4}
+            >
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Start Location"
+                    variant="outlined"
+                ></TextField>
+            </Grid>
+
+            <Grid 
+                item
+                xs={4}
+            >
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Destination Location"
+                    variant="outlined"
+                ></TextField>
+            </Grid>
+            
+            <Grid 
+                item
+                xs={2}
+            >
+                <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.squareButton}
+                    endIcon={<AddIcon />}
+                >
+                    Start Trip
+                </Button>
+            </Grid>
+        </Grid>
+
+            <iframe
+                className={classes.iframe}
+                loading="lazy"
+                allowFullScreen
+                src={`https://www.google.com/maps/embed/v1/view?key=${API_KEY.GOOGLE}
+                &center=-33.8569,151.2152`}>
+            </iframe>
+        </div>
     )
 }
 
