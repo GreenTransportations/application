54897bbf5b6d9778a604f3850631f1a9dc3b76d5
Alexander Leye <anley1@student.monash.edu>
2021-08-23 21:54:20 +1000

    Google Maps API Key for geocode and js active

--- webapp/src/data/api.key.js
+++ webapp/src/data/api.key.js
@@ -1,3 +1,4 @@
 export const API_KEY = {
-    GOOGLE: "AIzaSyDH3yAY1TJ22nd3o7Sz0Ks2ssJ3gEG5fkw"
+    GOOGLE: "AIzaSyDH3yAY1TJ22nd3o7Sz0Ks2ssJ3gEG5fkw",
+    GOOGLE_ALEX: "AIzaSyAyl4N_MGORkCknTTsLVYaVidQUL4_BCyM"
 }
