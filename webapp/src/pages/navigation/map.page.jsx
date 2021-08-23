e68226b407f9427576a22f7f15f8f0d0df2a229c
Alexander Leye <anley1@student.monash.edu>
2021-08-23 21:56:33 +1000

    use react-google-maps api for map init

--- webapp/src/pages/navigation/map.page.jsx
+++ webapp/src/pages/navigation/map.page.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 
 // Material UI Core Components
@@ -22,77 +23,110 @@ import { TextField } from '@material-ui/core';
 
 // Style
 const useStyles = makeStyles((theme) => ({
-    iframe: {
-        border:0,
-        width:"100%",
-        height:"90vh",
+    gmap: {
+        border: 0,
+        width: "100%",
+        height: "90vh",
     }
 }));
 
 
+const containerStyle = {
+    width: '100%',
+    height: '90vh'
+};
+
+
 const MapPage = () => {
     const classes = useStyles();
-    return (
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: `${API_KEY.GOOGLE_ALEX}`
+    });
+
+    const [map, setMap] = React.useState(null);
+
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        setMap(map);
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null);
+    }, [])
+
+    const center = {
+        lat: -34.397,
+        lng: 150.644,
+    };
+
+    return isLoaded ? (
         <div
-        style={{padding: "30px"}}
+            style={{ padding: "30px" }}
         >
-        <Grid
-            container
-            direction="row"
-            justify="space-around"
-            alignItems="center"
-            spacing={1}
-        >
+            <Grid
+                container
+                direction="row"
+                justify="space-around"
+                alignItems="center"
+                spacing={1}
+            >
 
-            <Grid 
-                item
-                xs={4}
-            >
-                <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Start Location"
-                    variant="outlined"
-                ></TextField>
-            </Grid>
-
-            <Grid 
-                item
-                xs={4}
-            >
-                <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Destination Location"
-                    variant="outlined"
-                ></TextField>
-            </Grid>
-            
-            <Grid 
-                item
-                xs={2}
-            >
-                <Button
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.squareButton}
-                    endIcon={<AddIcon />}
+                <Grid
+                    item
+                    xs={4}
                 >
-                    Start Trip
-                </Button>
-            </Grid>
-        </Grid>
+                    <TextField
+                        fullWidth
+                        id="outlined-basic"
+                        label="Start Location"
+                        variant="outlined"
+                    ></TextField>
+                </Grid>
 
-            <iframe
-                className={classes.iframe}
-                loading="lazy"
-                allowFullScreen
-                src={`https://www.google.com/maps/embed/v1/view?key=${API_KEY.GOOGLE}
-                &center=-33.8569,151.2152`}>
-            </iframe>
+                <Grid
+                    item
+                    xs={4}
+                >
+                    <TextField
+                        fullWidth
+                        id="outlined-basic"
+                        label="Destination Location"
+                        variant="outlined"
+                    ></TextField>
+                </Grid>
+
+                <Grid
+                    item
+                    xs={2}
+                >
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.squareButton}
+                        endIcon={<AddIcon />}
+                    >
+                        Start Trip
+                    </Button>
+                </Grid>
+            </Grid>
+
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                // className={classes.gmap}
+                center={center}
+                zoom={6}
+                onLoad={map => onLoad(map)}
+                onUnmount={onUnmount}
+            >
+                {}
+                <></>
+            </GoogleMap>
         </div>
-    )
+    ) : <></>
 }
 
-export default MapPage;
+export default React.memo(MapPage)
