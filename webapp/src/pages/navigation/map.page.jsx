69b9dfdeb03ea3a0e702fc9657b69d590b2c68e5
Alexander Leye <anley1@student.monash.edu>
2021-08-27 18:30:28 +1000

    Autocomplete working for Source location

--- webapp/src/pages/navigation/map.page.jsx
+++ webapp/src/pages/navigation/map.page.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { GoogleMap, useJsApiLoader, Autocomplete, LoadScript } from '@react-google-maps/api';
+// import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 
 // Material UI Core Components
@@ -12,7 +12,8 @@ import { API_KEY } from '../../data/api.key';
 
 // Material UI Icons
 import AddIcon from '@material-ui/icons/Add';
-import { TextField } from '@material-ui/core';
+import { TextField, Input } from '@material-ui/core';
+
 
 
 
@@ -35,103 +36,147 @@ const useStyles = makeStyles((theme) => ({
 const containerStyle = {
     width: '100%',
     height: '90vh',
-    
+
 };
 
 
 const MapPage = () => {
     const classes = useStyles();
+    const [autocomplete, setAutocomplete] = useState(null);
 
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: `${API_KEY.GOOGLE_ALEX}`
-    });
+    // const { isLoaded } = useJsApiLoader({
+    //     id: 'google-map-script',
+    //     googleMapsApiKey: `${API_KEY.GOOGLE_ALEX}`
+    // });
 
-    const [map, setMap] = React.useState(null);
+    const [map, setMap] = useState(null);
 
-    const onLoad = React.useCallback(function callback(map) {
-        const bounds = new window.google.maps.LatLngBounds();
-        map.fitBounds(bounds);
-        setMap(map);
-    }, [])
+    // const onLoad = useCallback(function callback(map) {
+    //     const bounds = new window.google.maps.LatLngBounds();
+    //     map.fitBounds(bounds);
+    //     setMap(map);
+    // }, [])
+    const onLoad = (map) => {
+
+    }
 
     const onUnmount = React.useCallback(function callback(map) {
         setMap(null);
     }, [])
 
-    const center = {
-        lat: -34.397,
-        lng: 150.644,
+    const onAutoLoad = (autocomplete) => {
+        console.log('autocomplete: ', autocomplete);
+        setAutocomplete(autocomplete);
+    }
+
+    const onAutoPlaceChanged = () => {
+        if (autocomplete !== null) {
+            console.log(autocomplete.getPlace())
+        } else {
+            console.log('Autocomplete is not loaded yet!')
+        }
+    }
+
+    // const center = {
+    //     lat: -34.397,
+    //     lng: 150.644,
+    // };
+    const centre = {
+        lat: -25.0270548,
+         lng: 115.1824598
     };
 
-    return isLoaded ? (
-        <div
-            style={{ padding: "30px" }}
-        >
-            <Grid
-                container
-                direction="row"
-                justify="space-around"
-                alignItems="center"
-                spacing={1}
-            >
+    // return (
+    //     <div>
+    //         <GooglePlacesAutocomplete
+    //             apiKey={API_KEY.GOOGLE_ALEX}
+    //         />
+    //     </div>
+    // )
 
+    // return isLoaded ? (
+    return (
+        <LoadScript id="script-loader" googleMapsApiKey={API_KEY.GOOGLE_ALEX} libraries={["places"]}>
+            <div
+                style={{ padding: "30px" }}
+            >
                 <Grid
-                    item
-                    xs={4}
+                    container
+                    direction="row"
+                    justify="space-around"
+                    alignItems="center"
+                    spacing={1}
                 >
-                    <GooglePlacesAutocomplete
+
+                    <Grid
+                        item
+                        xs={4}
+                    >
+                        {/* <GooglePlacesAutocomplete
                         apiKey={API_KEY.GOOGLE_ALEX}
                         // fullWidth
                         // id="outlined-basic"
                         // label="Start Location"
                         // variant="outlined"
-                    />
-                </Grid>
+                    /> */}
+                        <Autocomplete
+                            onLoad={onAutoLoad}
+                            onPlaceChanged={onAutoPlaceChanged}
+                        >
+                            <Input
+                                fullWidth
+                                id="outlined-basic"
+                                label="Start Location"
+                                variant="outlined"
+                            ></Input>
+                        </Autocomplete>
+                    </Grid>
 
-                <Grid
-                    item
-                    xs={4}
-                >
-                    <TextField
-                        fullWidth
-                        id="outlined-basic"
-                        label="Destination Location"
-                        variant="outlined"
-                    ></TextField>
-                </Grid>
-
-                <Grid
-                    item
-                    xs={2}
-                >
-                    <Button
-                        style = {{borderRadius: "180px"}}
-                        fullWidth
-
-                        variant="contained"
-                        color="primary"
-                        className={classes.squareButton}
-                        endIcon={<AddIcon />}
+                    <Grid
+                        item
+                        xs={4}
                     >
-                        Start Trip
-                    </Button>
-                </Grid>
-            </Grid>
+                        <TextField
+                            fullWidth
+                            id="outlined-basic"
+                            label="Destination Location"
+                            variant="outlined"
+                        ></TextField>
+                    </Grid>
 
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                // className={classes.gmap}
-                center={center}
-                zoom={6}
-                onLoad={map => onLoad(map)}
-                onUnmount={onUnmount}
-            >
-                {}
-                <></>
-            </GoogleMap>
-        </div>
-    ) : <></>
+                    <Grid
+                        item
+                        xs={2}
+                    >
+                        <Button
+                            style={{ borderRadius: "180px" }}
+                            fullWidth
+
+                            variant="contained"
+                            color="primary"
+                            className={classes.squareButton}
+                            endIcon={<AddIcon />}
+                        >
+                            Start Trip
+                        </Button>
+                    </Grid>
+                </Grid>
+
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    // className={classes.gmap}
+                    center={centre}
+                    zoom={5}
+                    onLoad={map => onLoad(map)}
+                    onUnmount={onUnmount}
+                >
+                    { }
+                    <></>
+                </GoogleMap>
+            </div>
+        </LoadScript>
+        // ) : <></>
+    )
 }
 
 export default React.memo(MapPage)
